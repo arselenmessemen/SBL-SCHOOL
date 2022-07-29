889601599cabf222e7119b7db91f2dad22cc3cd6
Arselen Messemen <88986123+arselenmessemen@users.noreply.github.com>
2022-07-29 15:45:59 +0100

    test

--- src.js
+++ src.js
@@ -4,11 +4,23 @@ var btnOpen = document.getElementById("btnOpen");
 var mq = window.screen.availWidth;
 btnClose.style.display = "none";
 btnOpen.style.display = "none";
+navlinks.style.display = "none";
 
-addEventListener("resize", function () {
-  if (window.screen.availWidth < 700) {
+addEventListener("load", function () {
+  if (window.screen.availWidth < 600) {
     navlinks.style.display = "none";
-    btnClose.style.display = "block";
+    btnClose.style.display = "none";
+    btnOpen.style.display = "block";
+  } else {
+    navlinks.style.display = "block";
+    btnClose.style.display = "none";
+    btnOpen.style.display = "none";
+  }
+});
+addEventListener("resize", function () {
+  if (window.screen.availWidth < 600) {
+    navlinks.style.display = "none";
+    btnClose.style.display = "none";
     btnOpen.style.display = "block";
   } else {
     navlinks.style.display = "block";
@@ -18,11 +30,10 @@ addEventListener("resize", function () {
 });
 
 function Open() {
-  console.log("OPEN");
   navlinks.style.display = "block";
+  btnClose.style.display = "block";
 }
 
 function Close() {
-  console.log("Close");
   navlinks.style.display = "none";
 }
